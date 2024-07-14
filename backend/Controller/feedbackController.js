985ed2fce41b41fe104b96451ec90b1e2ddef09c
Maaz <abumaaz728@gmail.com>
2024-07-14 18:25:13 +0530

    Created Contact/Feeddback Form API and Added Swagger functionality

---
+++ backend/Controller/feedbackController.js
@@ -0,0 +1,38 @@
+const Feedback = require("../Models/FeedbackModel");
+const { createFeedback } = require("../services/feedbackService");
+
+
+exports.addFeedback = async(req,res) => {
+
+    try {
+        const {email, feedback} = req.body;
+        if(!email || !feedback){
+            res.status(200).json({ message: "Email and feedback required" });
+        }
+
+        const createdFeedback = await createFeedback(req.body);
+        res.status(201).json(createdFeedback);
+
+
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+exports.getAllFeedbacks = async(_,res)=>{
+    try {
+        const feedbacks = await Feedback.find({});
+        if(!feedbacks){
+            return res.status(200).json({message:"No feedbacks found in database", feedbacks}); 
+            
+        }
+        return res.status(200).json({message:"All feedbacks fetched successfully", feedbacks}); 
+        
+        
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message || "Something went wrong while fetching feedbacks" });
+        
+    }
+}
--- backend/Routes/index.js
+++ backend/Routes/index.js
@@ -1,8 +1,11 @@
 const express=require('express')
-const router=express.Router();
+const router = express.Router();
 const VolunteerRoute=require('./volunteerRoute')
-const EventRouter=require('./EventRoute')
+const EventRouter=require('./EventRoute');
+const feedbackRoute = require('./feedbackRoute');
+
 router.use('/',VolunteerRoute);
 router.use('/event',EventRouter)
+router.use('/feedback',feedbackRoute);
 
 module.exports=router;
