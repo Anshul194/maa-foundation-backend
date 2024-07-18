607ee8dac90d1fcbbb2b66b12ff46a697b179d0a
Anshul <ananyaj138@gmail.com>
2024-07-18 12:48:24 +0530

    'Name changed'

--- Routes/feedbackRoute.js
+++ Routes/feedbackRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {
   addFeedback,
   getAllFeedbacks,
-} = require("../controller/FeedbackController");
+} = require("../controller/feedbackController");
 const feedbackRoute = express.Router();
 
 /**
