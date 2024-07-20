4d35ccbea8724643448666f36bd57ba30ebb1db8
Prrooo <harshmalik00011@gmail.com>
2024-07-20 21:26:47 +0530

    env change

--- backend/Routes/index.js
+++ backend/Routes/index.js
@@ -2,12 +2,10 @@ const express=require('express')
 const router = express.Router();
 const VolunteerRoute=require('./volunteerRoute')
 const EventRouter=require('./EventRoute')
-<<<<<<< HEAD
 const PayRouter=require('./payRoute');
 router.use('/',VolunteerRoute);
 router.use('/event',EventRouter)
 router.use('/payment',PayRouter);
-=======
 const BlogRouter=require('./BlogRoute')
 const SubscribeRouter=require('../Routes/SubscriptionRoute')
 
@@ -19,5 +17,4 @@ router.use('/',EventRouter)
 router.use('/feedback',feedbackRoute);
 router.use('/',SubscribeRouter);
 
->>>>>>> 4168692e3ac8fb80e578ece8384033aa602bfd46
 module.exports=router;
